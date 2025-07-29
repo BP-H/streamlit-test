76a9e6ace694bb9924e9d42cb3989314c4af38b5
BP-H <blueprinthustler@gmail.com>
2025-07-29 16:23:29 -0400

    Add modern card layout and update theme

--- app.py
+++ app.py
@@ -1,2 +1,8 @@
 import streamlit as st
+from streamlit_helpers import inject_global_styles
+
+inject_global_styles()
+
+st.markdown("<div class='card'>", unsafe_allow_html=True)
 st.title("It Works!")
+st.markdown("</div>", unsafe_allow_html=True)
